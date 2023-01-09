997be8ac69508226339512424cbd7f506d1bcf9d
Unreasonable324 <a8surd324@yandex.ru>
2023-01-09 15:47:28 +0700

    commit

---
+++ SendingForms/send.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+echo $fio;
+echo "<br>";
+echo $email;
+if (mail("a8surd324@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: shumak324@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
